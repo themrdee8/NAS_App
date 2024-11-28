7c2dd3fc0d7832e41f4b76130a96a348fad1c88f
Senyo Dedoo <mrdee@Senyos-MacBook-Air.local>
2024-11-28 15:03:17 +0000

    11: Light mode for status bar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,6 +6,7 @@ import { useEffect } from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { StatusBar } from 'expo-status-bar';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -32,6 +33,7 @@ export default function RootLayout() {
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
       </Stack>
+      <StatusBar backgroundColor='#0A1713' style='light' />
     </ThemeProvider>
   );
 }
